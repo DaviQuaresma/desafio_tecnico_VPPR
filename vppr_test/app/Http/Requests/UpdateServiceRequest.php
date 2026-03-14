e5a6436c8e4e8adf0c706607ea66f93dc1de38bc
Davi Quaresma <quaresma.davi@gmail.com>
2026-03-14 12:26:20 -0300

    feat: Add StoreServiceRequest and UpdateServiceRequest for service validation

---
+++ vppr_test/app/Http/Requests/UpdateServiceRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateServiceRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'value' => 'sometimes|required|numeric|min:0',
+            'description' => 'nullable|string',
+        ];
+    }
+}
