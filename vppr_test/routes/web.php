48d81283afe3b593fd8a85e6ae38a816cb163e70
Davi Quaresma <quaresma.davi@gmail.com>
2026-03-16 11:25:14 -0300

    feat: Add reset password route and views for registration and password recovery

--- vppr_test/routes/web.php
+++ vppr_test/routes/web.php
@@ -10,6 +10,14 @@ Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+})->name('password.request');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
